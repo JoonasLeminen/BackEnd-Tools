4713ce29750b9fdf781a92dd90057b0305d8e84a
Joonas Leminen <kaktus994hotmail.com>
2019-01-09 12:40:49 +0200

    Initial commit

--- WebApiExample/WebApiExample/Controllers/ValuesController.cs
+++ WebApiExample/WebApiExample/Controllers/ValuesController.cs
@@ -14,14 +14,14 @@ namespace WebApiExample.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "valueA", "valueB" };
+            return new string[] { "valueAA", "valueBB" };
         }
 
         // GET api/values/5
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
-            return "valueAB";
+            return "valueAABB";
         }
 
         // POST api/values
